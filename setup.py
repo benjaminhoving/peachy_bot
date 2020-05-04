3698fb18b3e3ff006855269eae2e1c878845da69
Benjamin Hoving <benjamin.hoving@gmail.com>
2020-05-04 19:01:58 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="PeachyBot",
+    version="0.1",
+    packages=find_packages(),
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=["discord.py"],
+
+    entry_points={
+        "console_scripts": [
+            "peachy = peachy.main",
+        ],
+    },
+
+    # metadata to display on PyPI
+    author="Benjamin Hoving",
+    author_email="benjamin.hoving@gmail.com",
+    description="Peachy's Bot on Discord",
+)
